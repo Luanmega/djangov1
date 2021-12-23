086147d637d44a965cdfe4c4081c350b5d2df275
Luanmega <medina1192@gmail.com>
2021-12-23 11:28:19 -0700

    Models created

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Productos(models.Model):
+    tienda = models.ForeignKey('tiendas.tiendas', on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=200)
+    precio = models.FloatField()
+    area = models.CharField(max_length=100)
+    identificador = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
--- tiendas/models.py
+++ tiendas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class tiendas(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=200)
+    direccion = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.nombre
