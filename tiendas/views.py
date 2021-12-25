89ea67872db1aa42519a0ff6f30dadaf8280ecca
Luanmega <medina1192@gmail.com>
2021-12-25 04:05:15 -0700

    Some landing content

--- tiendas/views.py
+++ tiendas/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import tiendas as Tiendas
+from productos.models import Productos
 
 def landing(request):
-    return render(request, 'tiendas/landing.html', {})
+    tiendas = Tiendas.objects.all()
+    productos = Productos.objects.all()
+
+    return render(request, 'tiendas/landing.html', { 'tiendas': tiendas, 'productos': productos })
 
 def tiendas(request):
     return render(request, 'tiendas/tiendas.html', {})
