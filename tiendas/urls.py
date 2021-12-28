27a39847bdfb4d9db136cbcccfcd6aa63102ef4a
Luanmega <medina1192@gmail.com>
2021-12-28 12:19:50 -0700

    Tiendas can add, update and delete. End of version 1.

--- tiendas/urls.py
+++ tiendas/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name='landing'),
-    path('tiendas', views.tiendas, name='tiendas'),
+    path('tiendas/<int:pk>/', views.tiendas, name='tiendas'),
     path('add_tienda', views.add_tienda, name='add_tienda'),
+    path('delete_tienda/<int:pk>/', views.delete_tienda, name='delete_tienda')
 ]
--- tiendas/views.py
+++ tiendas/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import tiendas as Tiendas
 from productos.models import Productos
 from django.http import HttpResponse
 import json
+
+#Use request.body when application/json
+#Use request.post when not applicacion/json
 
 def landing(request):
     tiendas = Tiendas.objects.all()
@@ -10,22 +13,37 @@ def landing(request):
 
     return render(request, 'tiendas/landing.html', { 'tiendas': tiendas, 'productos': productos })
 
-def tiendas(request):
+def tiendas(request, pk):
+    if pk != 0:
+        tienda = get_object_or_404(Tiendas, pk=pk)
+        return render(request, 'tiendas/tiendas.html', {'tienda' : tienda})
+    
     return render(request, 'tiendas/tiendas.html', {})
 
 def add_tienda(request):
-    print("PRINTO")
-    print(request.body)
     # if request is post and ajax 
     # if request.method == "POST":
-    tienda = Tiendas()
-    if request.method == 'POST':
+    if request.body:
         jsonResponse = json.loads(request.body.decode('utf-8'))
-        tienda.nombre = jsonResponse['nombre']
-        tienda.direccion = jsonResponse['direccion']
+        if jsonResponse['id'] != 0:
+            tienda = Tiendas()
+            tienda = get_object_or_404(Tiendas, pk=jsonResponse['id'])
+        else:
+            tienda = Tiendas()
 
-        tienda.save()
-        return HttpResponse("EXITO VIEW")     
+        if request.method == 'POST':
+            tienda.nombre = jsonResponse['nombre']
+            tienda.direccion = jsonResponse['direccion']
 
-    response = HttpResponse(request.body)
-    return response
+            tienda.save()
+
+            return redirect('landing')
+
+    return redirect('tiendas')
+
+def delete_tienda(request, pk):
+    tienda = Tiendas.objects.get(id=pk)
+    tienda.delete()
+
+    return redirect("landing")
+
